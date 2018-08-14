cd5b5b0d5ff6383cf4f86fed71506d67385ebb53
German Robayo Paz <germanrobayo33@gmail.com>
2018-08-14 19:39:44 -0400

    save static files local and configure webpack loaders

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import './index.css'
+import './plugins/bootstrap/css/bootstrap.min.css'
 import './css/orbit-1.css'
+import './plugins/fontawesome'
--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
-const webpack = require('webpack');
+const webpack = require('webpack')
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
@@ -27,6 +26,10 @@ module.exports = {
             {
                 test: /\.html/,
                 use: ['html-loader']
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/,
+                use: ['file-loader']
             }
         ]
     },
@@ -37,6 +40,7 @@ module.exports = {
             template: './src/index.html',
             minify: true
         }),
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new FaviconsWebpackPlugin('./src/icon.png')
     ]
 };
